68499deb07d5e0a889a0a3140591b97b3a66848a
YaGiNA <yutachihaya@gmail.com>
2017-04-14 20:22:49 +0900

    Complete to add function required by mission

--- app/main.py
+++ app/main.py
@@ -1,4 +1,35 @@
 #!/usr/bin/env python3
+import requests
+import sys
+
+url = 'http://challenge-server.code-check.io/api/recursive/ask'
+cache = {}
+
+
+def askServer(n, seed):
+    if n not in cache:
+        query = {"seed": seed, "n": n}
+        r = requests.get(url, params=query).json()
+        cache[n] = r["result"]
+    return cache[n]
+
+
+def call(n, seed):
+    if n == 0:
+        return 1
+    elif n == 2:
+        return n
+    elif n % 2 == 0:
+        nlist = [call(i, seed) for i in range(n-1, n-5, -1)]
+        return sum(nlist)
+    else:
+        return askServer(n, seed)
+
+
 def main(argv):
-  for v in argv:
-    print(v)
+    try:
+        seed = argv[0]
+        print(call(int(argv[1]), seed))
+    except IndexError:
+        print("Values is not inputed correctly.")
+        sys.exit(1)
